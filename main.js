cd1f28f524fca0f6a2befc6bb6a0635a4e6ddd0f
Eugene <kulinich.eugene.2412@gmail.com>
2023-08-13 22:01:09 +0600

    fix for js

--- main.js
+++ main.js
@@ -1,23 +1,107 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var aviary = require("./aviary");
-var biome_1 = require("./biome");
-var food_1 = require("./food");
+var Biome;
+(function (Biome) {
+    Biome[Biome["Tundra"] = 0] = "Tundra";
+    Biome[Biome["Taiga"] = 1] = "Taiga";
+    Biome[Biome["Forest"] = 2] = "Forest";
+    Biome[Biome["Grasslands"] = 3] = "Grasslands";
+    Biome[Biome["Desert"] = 4] = "Desert";
+    Biome[Biome["Plateau"] = 5] = "Plateau";
+    Biome[Biome["Water"] = 6] = "Water";
+    Biome[Biome["Home"] = 7] = "Home";
+})(Biome || (Biome = {}));
+
+var Food;
+(function (Food) {
+    Food[Food["Meat"] = 0] = "Meat";
+    Food[Food["Animal"] = 1] = "Animal";
+    Food[Food["Grass"] = 2] = "Grass";
+    Food[Food["Trees"] = 3] = "Trees";
+    Food[Food["People"] = 4] = "People";
+})(Food || (Food = {}));
+
+function Add(aviary, animal) {
+    if (aviary.Animals.some(x=> x.Biome !== animal.Biome)) {
+        console.log("another biome");
+        return;
+    }
+    if (aviary.Animals.some(x=> x.IsHerbivore !== animal.IsHerbivore)) {
+        console.log("different types of animals");
+        return;
+    }
+    if (aviary.Water - animal.NeedWater < 0) {
+        console.log("not enough water");
+        return;
+    }
+    if (aviary.Square - animal.NeedSquare < 0) {
+        console.log("not enough square");
+        return;
+    }
+
+    aviary.Water -= animal.NeedWater
+    aviary.Square -= animal.NeedSquare
+
+    aviary.Animals.push(animal)
+    console.log("animal is added");
+}
+
+function IsHerbivore(){
+    return this.Eats !== Food.Meat
+}
+
 var home = {
-    Biome: biome_1.Biome.Home,
-    Water: 10,
-    Square: 10,
-    Eat: 10,
-    Animals: undefined
+    Biome: Biome.Home,
+    Water: 100,
+    Square: 100,
+    Eat: 100,
+    Animals: []
 };
-var hippopotamus = {
-    Name: 'Moto-Moto',
-    Biome: biome_1.Biome.Tundra,
+
+const cat = {
+    Name: 'Zhorik',
+    Biome: Biome.Home,
     NeedWater: 10,
     NeedSquare: 2,
-    Eats: food_1.Food.Grass,
+    Eats: Food.Meat,
     IsHerbivore: function () {
-        return this.IsHerbivore();
+        return this.IsHerbivore()
+    }
+}
+
+var hippopotamus = {
+    Name: 'Moto-Moto',
+    Biome: Biome.Tundra,
+    NeedWater: 10,
+    NeedSquare: 2,
+    Eats: Food.Grass,
+    IsHerbivore: function () {
+        return IsHerbivore();
     }
 };
-aviary.Add(home, hippopotamus);
+
+const horse = {
+    Name: 'Yesaul',
+    Biome: Biome.Grasslands,
+    NeedWater: 2,
+    NeedSquare: 2,
+    Eats: Food.Grass,
+    IsHerbivore: function () {
+        return IsHerbivore();
+    }
+}
+
+const dog = {
+    Name: 'Sharik',
+    Biome: Biome.Home,
+    NeedWater: 10,
+    NeedSquare: 2,
+    Eats: Food.Meat,
+    IsHerbivore: function () {
+        return this.IsHerbivore()
+    }
+}
+
+
+Add(home, cat);
+Add(home, hippopotamus);
+Add(home, horse);
+Add(home, dog);
